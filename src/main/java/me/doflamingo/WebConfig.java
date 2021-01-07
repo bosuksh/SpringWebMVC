737ee09040ec0b3e3cf8f71ce427dbb2e1053357
bosuklsh <bosuksh@gmail.com>
2021-01-07 10:06:52 +0900

    feat: Make DispatcherServlet
    DispatcherServlet이 관리하는 ServletWebApplicationContext는 web에 관련된 즉 Controller만 빈으로 가지고 있고 RootWebApplicationContext는 나머지빈들을 가지고 있다(Service,Repository등)
    그래서 DispatcherServlet을 생성해서 /app 으로 들어오는 url은 dispatcherServlet이 받도록 설정

--- src/main/java/me/doflamingo/WebConfig.java
+++ src/main/java/me/doflamingo/WebConfig.java
@@ -1,11 +1,10 @@
 package me.doflamingo;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class))
-public class AppConfig {
+@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
+public class WebConfig {
 }
