d60067ec830c97b8269da2ea99285f7dd67b9f6b
Lee SangHyup <bosuksh@gmail.com>
2021-01-09 10:21:48 +0900

    feat: Add SpringBoot Project
    스프링부트 프로젝트를 추가

---
+++ springboot-mvc/src/test/java/me/doflamingo/springbootwebmvc/SpringbootWebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doflamingo.springbootwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebmvcApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
