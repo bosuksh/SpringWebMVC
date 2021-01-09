3359bfaebcd78473a57e8e8045c2716ba20828e2
Lee SangHyup <bosuksh@gmail.com>
2021-01-09 10:09:33 +0900

    fix: Change Project Path
    스프링부트 프로젝트를 추가하기 위해 프로젝트 위치 변경

--- spring-mvc/src/main/java/me/doflamingo/AppConfig.java
+++ spring-mvc/src/main/java/me/doflamingo/AppConfig.java
@@ -1,6 +1,5 @@
 package me.doflamingo;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
--- spring-mvc/src/main/java/me/doflamingo/WebConfig.java
+++ spring-mvc/src/main/java/me/doflamingo/WebConfig.java
@@ -1,14 +1,11 @@
 package me.doflamingo;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(useDefaultFilters = false , includeFilters = @ComponentScan.Filter(Controller.class))
