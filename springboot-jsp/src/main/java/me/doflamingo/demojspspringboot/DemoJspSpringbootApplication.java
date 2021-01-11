e54a0f235fa2b7e7c3a8289128d38a9338ae9bdf
Lee SangHyup <bosuksh@gmail.com>
2021-01-11 23:05:50 +0900

    feat: Add Springboot jsp project
    스프링부트에서 jsp 사용예제 추가

---
+++ springboot-jsp/src/main/java/me/doflamingo/demojspspringboot/DemoJspSpringbootApplication.java
@@ -0,0 +1,13 @@
+package me.doflamingo.demojspspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJspSpringbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoJspSpringbootApplication.class, args);
+  }
+
+}
---
+++ springboot-jsp/src/test/java/me/doflamingo/demojspspringboot/DemoJspSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doflamingo.demojspspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJspSpringbootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
