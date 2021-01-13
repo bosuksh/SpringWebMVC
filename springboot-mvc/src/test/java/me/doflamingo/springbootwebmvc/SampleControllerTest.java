f3bc012bfc5601ab4c2230279d379dedb9cb59e4
Lee SangHyup <bosuksh@gmail.com>
2021-01-13 23:53:25 +0900

    fix: Register PersonFormatter as a Bean
    WebConfig에서 registry 삭제 및 PersonFormatter 빈으로 등록
    그리고 WebMvcTest는 웹에 관련된 것만 슬라이싱 테스트하기 때문에 웹과 관련 없는건 제외됨
    그래서 @Component는 빈으로 등록이 안됨 그래서 통합테스트로 변경

--- springboot-mvc/src/test/java/me/doflamingo/springbootwebmvc/SampleControllerTest.java
+++ springboot-mvc/src/test/java/me/doflamingo/springbootwebmvc/SampleControllerTest.java
@@ -3,7 +3,9 @@ package me.doflamingo.springbootwebmvc;
 import me.doflamingo.springbootwebmvc.event.EventService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -11,7 +13,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 class SampleControllerTest {
 
   @Autowired
