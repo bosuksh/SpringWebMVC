7c67536dbecfbfba7ce84fb2e6e1c9e23c95d937
Lee SangHyup <bosuksh@gmail.com>
2021-01-15 00:13:40 +0900

    feat: Add ResourceHandler
    ResourceHandler를 이용하면 static Resource의 경로를 지정할 수 있고 마찬가지로 WebConfig에 등록하면 된다.
    그리고 cache등 다양한 전략을 적용할 수 있다.

--- springboot-mvc/src/main/java/me/doflamingo/springbootwebmvc/WebConfig.java
+++ springboot-mvc/src/main/java/me/doflamingo/springbootwebmvc/WebConfig.java
@@ -3,8 +3,12 @@ package me.doflamingo.springbootwebmvc;
 import me.doflamingo.springbootwebmvc.person.AnotherInterceptor;
 import me.doflamingo.springbootwebmvc.person.GreetingInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -15,5 +19,16 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addInterceptor(new AnotherInterceptor())
       .order(0)
       .addPathPatterns("/hello/*");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+      .addResourceHandler("/mobile/**")
+      .addResourceLocations("classpath:/mobileResource/")
+      .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES))
+      .setCachePeriod(100)
+      .resourceChain(true);
+
   }
 }
--- springboot-mvc/src/test/java/me/doflamingo/springbootwebmvc/SampleControllerTest.java
+++ springboot-mvc/src/test/java/me/doflamingo/springbootwebmvc/SampleControllerTest.java
@@ -3,21 +3,19 @@ package me.doflamingo.springbootwebmvc;
 import me.doflamingo.springbootwebmvc.event.EventService;
 import me.doflamingo.springbootwebmvc.person.Person;
 import me.doflamingo.springbootwebmvc.person.PersonRepository;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -57,5 +55,21 @@ class SampleControllerTest {
       .andExpect(content().string("Hello name"));
   }
 
+  @Test
+  public void helloStatic() throws Exception {
+    mockMvc.perform(get("/index.html"))
+      .andDo(print())
+      .andExpect(status().isOk())
+      .andExpect(content().string(Matchers.containsString("Hello Index")));
+  }
+
+  @Test
+  public void resourceHandler() throws Exception {
+    mockMvc.perform(get("/mobile/index.html"))
+      .andDo(print())
+      .andExpect(status().isOk())
+      .andExpect(content().string(Matchers.containsString("Hello Mobile")))
+      .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
+  }
 
 }
