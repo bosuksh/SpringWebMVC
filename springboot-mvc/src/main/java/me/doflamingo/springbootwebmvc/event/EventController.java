578f3bd07e1b8c02d3f72e6ee6fdb329d892ba1e
Lee SangHyup <bosuksh@gmail.com>
2021-01-19 06:43:03 +0900

    feat: Add Exercise Test
    연습문제로 다양한 요청이 들어왔을 때 처리

--- springboot-mvc/src/main/java/me/doflamingo/springbootwebmvc/event/EventController.java
+++ springboot-mvc/src/main/java/me/doflamingo/springbootwebmvc/event/EventController.java
@@ -1,8 +1,9 @@
 package me.doflamingo.springbootwebmvc.event;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class EventController {
@@ -13,9 +14,39 @@ public class EventController {
     this.eventService = eventService;
   }
 
-  @GetMapping("/events")
+  @GetMapping("/event")
   public String getList(Model model) {
     model.addAttribute("events", eventService.getList());
     return "events/list";
+  }
+
+  @GetMapping("/events")
+  @ResponseBody
+  public String getEvents(){
+    return "events";
+  }
+
+  @RequestMapping(value = "/events/?", method = {RequestMethod.GET, RequestMethod.DELETE})
+  @ResponseBody
+  public String getEventsWithOneCharacter(){
+    return "events";
+  }
+
+  @PostMapping(
+    value = "/events",
+    consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseBody
+  public String postEventsWithJson() {
+    return "/events";
+  }
+
+  @PutMapping(
+    value = "/events/{id}",
+    consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseBody
+  public String putEventsWithJson(@PathVariable Long id) {
+    return "/events "+ id;
   }
 }
---
+++ springboot-mvc/src/test/java/me/doflamingo/springbootwebmvc/event/EventControllerTest.java
@@ -0,0 +1,115 @@
+package me.doflamingo.springbootwebmvc.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class EventControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @MockBean
+  EventService eventService;
+
+  @Test
+  public void eventTest() throws Exception {
+    mockMvc.perform(get("/events"))
+      .andDo(print())
+      .andExpect(status().isOk());
+  }
+
+  @Test
+  public void get_eventTest_with_one_character() throws Exception {
+    mockMvc.perform(get("/events/1"))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+
+    mockMvc.perform(get("/events/2"))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+
+    mockMvc.perform(get("/events/3"))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+
+    mockMvc.perform(get("/events/4"))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+  }
+
+  @Test
+  public void eventTest_with_contentType_and_acceptHeader() throws Exception {
+    mockMvc.perform(post("/events")
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON))
+      .andDo(print())
+      .andExpect(status().isOk());
+  }
+
+  @Test
+  public void delete_eventTest_with_one_character() throws Exception {
+    mockMvc.perform(delete("/events/1"))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+
+    mockMvc.perform(delete("/events/2"))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+
+    mockMvc.perform(delete("/events/3"))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+
+    mockMvc.perform(delete("/events/4"))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+  }
+
+  @Test
+  public void put_eventTest_with_contentType_and_acceptHeader() throws Exception {
+    mockMvc.perform(put("/events/1")
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+
+    mockMvc.perform(put("/events/2")
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+
+    mockMvc.perform(put("/events/3")
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+
+    mockMvc.perform(put("/events/4")
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON))
+      .andDo(print())
+      .andExpect(status().isOk())
+    ;
+  }
+}
