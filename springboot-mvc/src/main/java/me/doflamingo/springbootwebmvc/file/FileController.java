ddc9fae8d43703e803a2d908b54399b262e201b8
Lee SangHyup <bosuksh@gmail.com>
2021-01-23 20:08:43 +0900

    feat: Add FileDownload

--- springboot-mvc/src/main/java/me/doflamingo/springbootwebmvc/file/FileController.java
+++ springboot-mvc/src/main/java/me/doflamingo/springbootwebmvc/file/FileController.java
@@ -1,15 +1,28 @@
 package me.doflamingo.springbootwebmvc.file;
 
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class FileController {
+
+  @Autowired
+  private ResourceLoader resourceLoader;
 
   @GetMapping("/files")
   public String getFileForm(Model model) {
@@ -24,5 +37,18 @@ public class FileController {
     String message = file.getOriginalFilename()+" is uploaded.";
     redirectAttributes.addFlashAttribute("message", message);
     return "redirect:/files";
+  }
+
+  @GetMapping("/files/{fileName}")
+  public ResponseEntity<?> downloadFile(@PathVariable String fileName) throws IOException {
+    Resource resource = resourceLoader.getResource("classpath:"+fileName);
+    File file = resource.getFile();
+    Tika tika = new Tika();
+    String mediaType = tika.detect(file);
+    return ResponseEntity.ok()
+      .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\""+fileName+"\"")
+      .header(HttpHeaders.CONTENT_TYPE,mediaType)
+      .header(HttpHeaders.CONTENT_LENGTH, file.length()+"")
+      .body(resource);
   }
 }
